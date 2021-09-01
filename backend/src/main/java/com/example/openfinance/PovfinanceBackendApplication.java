0aca0b390fea8d8f727ff32af594be0872b361ab
juliano <thespamer@gmail.com>
2021-09-01 15:17:22 +0100

    Mensagem do commit

---
+++ backend/src/main/java/com/example/openfinance/PovfinanceBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.openfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovfinanceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PovfinanceBackendApplication.class, args);
+    }
+}
