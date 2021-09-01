226f1878c899b6110b415e497c4734df049e6714
juliano <thespamer@gmail.com>
2021-09-01 15:17:22 +0100

    Mensagem do commit

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Ensure this path is correct
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
