a09e499a2b8786eeba57d30ca3c610c381cc5b6f
Juliano Souza <46322485+thespamer@users.noreply.github.com>
2024-09-01 17:34:44 -0300

    Update populate_db.sql

--- scripts/populate_db.sql
+++ scripts/populate_db.sql
@@ -1,17 +1,17 @@
--- Create the database
-CREATE DATABASE open_finance_db;
+-- Conectar-se ao banco de dados, caso esteja utilizando o psql
+\c open_finance_db;
 
--- Connect to the database
-\\c open_finance_db;
+-- Verifique se a tabela já existe, e se sim, exclua-a
+DROP TABLE IF EXISTS account;
 
--- Create the Account table
+-- Crie a tabela Account
 CREATE TABLE account (
     id SERIAL PRIMARY KEY,
     account_number VARCHAR(20) NOT NULL UNIQUE,
     balance DECIMAL(15, 2) NOT NULL DEFAULT 0.00
 );
 
--- Insert initial data
+-- Inserir dados iniciais
 INSERT INTO account (account_number, balance) VALUES ('123456', 1000.0);
 INSERT INTO account (account_number, balance) VALUES ('654321', 2000.0);
 INSERT INTO account (account_number, balance) VALUES ('345678', 1000.0);
